892c73e55bffb57d90bfb63442430f334609eafe
Enrico R <enricorrx@gmail.com>
2021-04-06 17:57:17 +0200

    Added comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
+    // load vars from file
     private static Properties loadProps() throws IOException {
         final var in = Main.class.getResourceAsStream("data.properties");
         if (in == null) {
