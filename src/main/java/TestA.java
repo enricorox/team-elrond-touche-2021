4a4df053b529a5f84072941b6891714f543039af
Gianmarco C <giamgiammi@gmail.com>
2021-04-11 13:42:52 +0200

    Analyzer manual test + wordnet synonyms inclusion example

---
+++ src/main/java/TestA.java
@@ -0,0 +1,32 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.StopFilterFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilterFactory;
+import org.apache.lucene.analysis.synonym.WordnetSynonymParser;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
+public class TestA {
+    public static void main(String[] args) throws Exception {
+        final var synonFilterMap = new TreeMap<String, String>();
+        synonFilterMap.put("synonyms", "wn_s.pl");
+        synonFilterMap.put("format", "wordnet");
+        System.out.println(ClassLoader.getSystemResource("wn_s.pl"));
+        final Analyzer a = CustomAnalyzer.builder()
+                .withTokenizer(StandardTokenizerFactory.class)
+                .addTokenFilter(LowerCaseFilterFactory.class)
+                .addTokenFilter(SynonymGraphFilterFactory.class, synonFilterMap)
+                .addTokenFilter(StopFilterFactory.class)
+                .build();
+        var s = a.tokenStream("body", new InputStreamReader(System.in));
+        CharTermAttribute att = s.getAttribute(CharTermAttribute.class);
+        s.reset();
+        while (s.incrementToken()) {
+            System.out.println(att.toString());
+        }
+    }
+}
