7617edc4b1e39f022affd35f608dc7cc14fc3ad2
Gianmarco C <giamgiammi@gmail.com>
2021-04-30 13:01:23 +0200

    changes for TIRA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import utils.Props;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.Properties;
 
 public class Main {
@@ -14,6 +19,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         final var props = loadProps();
+        parseOptionAndEditProps(args, props);
 
         final int ramBuffer = 256;
 
@@ -51,17 +57,15 @@ public class Main {
 
         final long startTime = System.currentTimeMillis();
 
-        if (args.length >= 1) {
-            switch(args[0]) {
-                case "OpenNlpRun" -> PreparedRuns.OPEN_NLP.execute(data);
-                case "TaskBodyRun" -> PreparedRuns.TASK_BODY_SEARCHER.execute(data);
-                case "KRun" -> PreparedRuns.K_RUN.execute(data);
-                case "SimpleRun" -> PreparedRuns.SIMPLE_RUN.execute(data);
-                default -> {
-                    errorAndPrintHelp("Unknown run name");
-                }
-            };
-        } else errorAndPrintHelp("No run specified");
+        switch(props.getProperty("RunToExecute")) {
+            case "OpenNlpRun" -> PreparedRuns.OPEN_NLP.execute(data);
+            case "TaskBodyRun" -> PreparedRuns.TASK_BODY_SEARCHER.execute(data);
+            case "KRun" -> PreparedRuns.K_RUN.execute(data);
+            case "SimpleRun" -> PreparedRuns.SIMPLE_RUN.execute(data);
+            default -> {
+                throw new IllegalArgumentException("Command line parsing messed up");
+            }
+        }
 
         final long endTime = System.currentTimeMillis();
 
@@ -71,11 +75,48 @@ public class Main {
     private static void errorAndPrintHelp(final String error) {
         System.err.println(error);
         System.err.println("""
-                HELP: execute this program passing a single parameter on the command line with the name of the run.
-                Possible runs:
+                Usage: this_program [-i input folder] [-o output dir] run_name
+                -i     Replace input directory (for TIRA)
+                -o     Replace output directory (for TIRA)
+                Possible run names:
                                SimpleRun
                                KRun
                                TaskBodyRun
                                OpenNlpRun""");
+    }
+
+    private static void parseOptionAndEditProps(final String[] args, final Properties props) {
+        final var cmdParser = new DefaultParser();
+        final var options = new Options();
+
+        options.addOption("i", true, "Input dir for both dataset and topics");
+        options.addOption("o", true, "Output dir");
+
+        CommandLine cmd = null;
+        try {
+            cmd = cmdParser.parse(options, args, true);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        cmd = Objects.requireNonNull(cmd);
+
+        if (cmd.getArgs().length != 1) {
+            errorAndPrintHelp("Unrecognised run");
+            System.exit(1);
+        }
+        props.setProperty("RunToExecute", cmd.getArgs()[0]);
+
+        final var i = cmd.getOptionValue("i");
+        final var o = cmd.getOptionValue("o");
+        if (i != null) {
+            System.out.println("Replacing input props value with command line options...");
+            props.setProperty("docs_path", i);
+            props.setProperty("topics_path", i + "/" + "topics.xml");
+        }
+        if (o != null) {
+            System.out.println("Replacing output props with command line option");
+            props.setProperty("work_folder", o);
+        }
     }
 }
